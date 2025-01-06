30ded53f41c89749d2f0c161560296914100411b
Divyanshrivastava <112326849+Divyanshrivastava@users.noreply.github.com>
2025-01-06 19:02:52 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import { FaLocationArrow } from "react-icons/fa6";
 export const Hero = () => {
   return (
+    <section id='hero'>
     <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 relative overflow-clip">
       <div className="mt-2 absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] lg:h-[550px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
       <div className="container relative">
@@ -27,6 +28,7 @@ export const Hero = () => {
         </div>
       </div>
     </div>
+    </section>
   );
 };
 
